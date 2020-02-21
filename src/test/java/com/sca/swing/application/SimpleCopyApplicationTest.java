8df8c19cda6de59d2da06a7d855c4ebc6be5add8
Serguei Cambour <serguei.cambour@infrabel.be>
2020-02-21 11:34:28 +0100

    Create a test class for the SimpleCopyApplication

---
+++ src/test/java/com/sca/swing/application/SimpleCopyApplicationTest.java
@@ -0,0 +1,37 @@
+package com.sca.swing.application;
+
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SimpleCopyApplicationTest {
+    private FrameFixture window;
+
+    @BeforeClass
+    public static void setUpOnce() {
+        FailOnThreadViolationRepaintManager.install();
+    }
+
+    @Before
+    public void setUp() {
+        SimpleCopyApplication frame = GuiActionRunner.execute(() -> new SimpleCopyApplication());
+        window = new FrameFixture(frame);
+        window.show(); // shows the frame to test
+    }
+
+    @Test
+    public void shouldCopyTextInLabelWhenClickingButton() {
+        window.textBox("textToCopy").enterText("Some random text");
+        window.button("copyButton").click();
+        window.label("copiedText").requireText("Some random text");
+    }
+
+    @After
+    public void tearDown() {
+        window.cleanUp();
+    }
+}
